5db431e5f6310f7d640c1a62822e0fc9b1e76d87
Pradeep <pradeep.wadhwa14@gmail.com>
2020-11-27 15:27:32 +0530

    Sorting issue

--- resources/views/FrontEnd/devices/devicesSorting.blade.php
+++ resources/views/FrontEnd/devices/devicesSorting.blade.php
@@ -17,48 +17,50 @@
             <td><a class="form-control btn table-row-btn" href="{{url('/signup')}}">{{__('deviceresult.signup_unlock')}}</td>
         @endif
     </tr>
-    @if($i%10 == 0)
-        @if($custom_ads === 0)
-            @if($j < count($ads))
-                @php $custom_ads = 1; @endphp
-                <tr class="custom-row-cl @if($i%10 == 0) adds @endif">
-                    <td colspan="7">
-                        <div class="add text-center">
-                            
-                            <img src="{{URL::asset('ads_banner/resized')}}/{{$ads[$j]['ads_file']}}"/>
-                        </div>
-                    </td>
-                </tr>
-                @php $j++; @endphp
-            @else
-                @php $custom_ads = 1; $j = 0;@endphp
-                <tr class="custom-row-cl @if($i%10 == 0) adds @endif">
-                    <td colspan="7">
-                        <div class="add text-center">
-                            
-                            <img src="{{URL::asset('ads_banner/resized')}}/{{$ads[$j]['ads_file']}}"/>
-                        </div>
-                    </td>
-                </tr>
-                @php $j++; @endphp
-            @endif
-        @else
-            @php $custom_ads = 0; @endphp
-            @if($googleads)
-                @if($googleads->script != "")
-                <tr class="custom-row-cl adds">
-                    <td colspan="7">
-                        <div class="row align-items-center">
-                            {!!$googleads->script!!}
-                            <!-- <div class="col-lg-6 text-center">
-                                <img src="{{URL::asset('frontend/assets/img/case.jpg')}}"/>
+    @if(count($ads) > 0)
+        @if($i%10 == 0)
+            @if($custom_ads === 0)
+                @if($j < count($ads))
+                    @php $custom_ads = 1; @endphp
+                    <tr class="custom-row-cl @if($i%10 == 0) adds @endif">
+                        <td colspan="7">
+                            <div class="add text-center">
+                                
+                                <img src="{{URL::asset('ads_banner/resized')}}/{{$ads[$j]['ads_file']}}"/>
                             </div>
-                            <div class="col-lg-6">
-                                <h1 class="adds-text">The Ultimate cover</h1>
-                            </div> -->
-                        </div>
-                    </td>
-                </tr> 
+                        </td>
+                    </tr>
+                    @php $j++; @endphp
+                @else
+                    @php $custom_ads = 1; $j = 0;@endphp
+                    <tr class="custom-row-cl @if($i%10 == 0) adds @endif">
+                        <td colspan="7">
+                            <div class="add text-center">
+                                
+                                <img src="{{URL::asset('ads_banner/resized')}}/{{$ads[$j]['ads_file']}}"/>
+                            </div>
+                        </td>
+                    </tr>
+                    @php $j++; @endphp
+                @endif
+            @else
+                @php $custom_ads = 0; @endphp
+                @if($googleads)
+                    @if($googleads->script != "")
+                    <tr class="custom-row-cl adds">
+                        <td colspan="7">
+                            <div class="row align-items-center">
+                                {!!$googleads->script!!}
+                                <!-- <div class="col-lg-6 text-center">
+                                    <img src="{{URL::asset('frontend/assets/img/case.jpg')}}"/>
+                                </div>
+                                <div class="col-lg-6">
+                                    <h1 class="adds-text">The Ultimate cover</h1>
+                                </div> -->
+                            </div>
+                        </td>
+                    </tr> 
+                    @endif
                 @endif
             @endif
         @endif

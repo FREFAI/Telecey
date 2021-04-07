3aefd99f381a49a9ddcb20460457153970be8c81
Tajinder <tejinder.upwork@gmail.com>
2021-04-07 14:09:55 +0530

    Css changes

--- resources/views/FrontEnd/Terms/Cookie.blade.php
+++ resources/views/FrontEnd/Terms/Cookie.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.frontend_layouts.frontend')
 @section('title', 'Cookie Policy')
 @section('content')
+<style type="text/css">
+	.review-container {
+	    margin-top:10px;
+	    margin-bottom:10px;
+	}
+
+</style>
 	<!-- Content Start Here -->
 		<div class="inner-page start-page" style="background: url({{URL::asset('frontend/assets/img/bg-1.jpeg')}});">
 		    <div class="container-fluid">

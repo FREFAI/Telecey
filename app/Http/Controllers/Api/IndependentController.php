ff6af487365a785185f186576c75acddcbdb0562
Jatinder <jatinder.kumar@softradix.com>
2021-05-19 16:38:00 +0530

    Import Device functionality

--- app/Http/Controllers/Api/IndependentController.php
+++ app/Http/Controllers/Api/IndependentController.php
@@ -25,34 +25,33 @@ class IndependentController extends Controller
         }
         else 
         {
-            $ModelsData = [];
+            
             try {
-                Excel::load(Input::file('file'), function ($reader) {
-                    $i = 0;
-                    foreach ($reader->toArray() as $row) {
-                        $now = date("Y-m-d H:i:s");
-                        if($row['device_type'] == 'Mobile Phone'){
-                            $type = 8;
-                        }else{
-                            $type = 12;
-                        }
-                        $model = [
-                            'brand_name'    => $row['developer'],
-                            'model_name'    => $row['model'],
-                            'device_type'   => $type,
-                            'colors_id'     => null,
-                            'user_id'       => 0,
-                            'default'       => 0,
-                            'status'        => 1,
-                            'created_at'    => $now,
-                            'updated_at'    => $now
-                        ];
-                        $ModelsData[$i] = $model;
-                        $i++;
+                $path = Input::file('file');
+                $data = Excel::load($path)->get();
+                $ModelsData = [];
+                foreach ($data->toArray() as $row) {
+                    $now = date("Y-m-d H:i:s");
+                    if($row['device_type'] == 'Mobile Phone'){
+                        $type = 8;
+                    }else{
+                        $type = 12;
                     }
-                    Brands::insert($ModelsData);
-                    return json_encode(['success'=> true, 'message'=> 'Data import']);
-                });
+                    $model = [
+                        'brand_name'    => $row['developer'],
+                        'model_name'    => $row['model'],
+                        'device_type'   => $type,
+                        'colors_id'     => null,
+                        'user_id'       => 0,
+                        'default'       => 0,
+                        'status'        => 1,
+                        'created_at'    => $now,
+                        'updated_at'    => $now
+                    ];
+                    array_push($ModelsData, $model);
+                }
+                Brands::insert($ModelsData);
+                return json_encode(['success'=> true, 'message'=> 'Data import', 'data'=> $data]);
                 
             } catch (\Exception $e) {
                 return json_encode(['success'=> false, 'message'=> 'Somthing went wrong', 'error'=> $e]);
